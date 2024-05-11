ddb0b6d60ebfa1a2766fdda8188ea6888501783e
Dusbchek <128949285+Dusbchek@users.noreply.github.com>
2024-05-10 22:12:59 -0600

    Update Program.cs

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using MongoDB.Bson;
 using MongoDB.Driver;
@@ -31,7 +31,9 @@ namespace MongoDBExample
                 Console.WriteLine("2. Buscar marca por producto");
                 Console.WriteLine("3. Eliminar producto");
                 Console.WriteLine("4. Modificar atributos de producto");
-                Console.WriteLine("5. Salir");
+                Console.WriteLine("5. Buscar producto por nombre");
+                Console.WriteLine("6. Buscar marca por nombre");
+                Console.WriteLine("7. Salir");
 
                 int opcion;
                 if (int.TryParse(Console.ReadLine(), out opcion))
@@ -51,6 +53,12 @@ namespace MongoDBExample
                             ModificarProducto();
                             break;
                         case 5:
+                            BuscarProductoPorNombre();
+                            break;
+                        case 6:
+                            BuscarMarcaPorNombre();
+                            break;
+                        case 7:
                             return;
                         default:
                             Console.WriteLine("Opción inválida. Intente de nuevo.");
@@ -156,6 +164,44 @@ namespace MongoDBExample
             }
         }
 
+        static void BuscarProductoPorNombre()
+        {
+            Console.WriteLine("Ingrese el nombre del producto:");
+            string nombreProducto = Console.ReadLine();
+
+            var filter = Builders<BsonDocument>.Filter.Eq("Nombre", nombreProducto);
+            var productoDocument = productosCollection.Find(filter).FirstOrDefault();
+
+            if (productoDocument != null)
+            {
+                Console.WriteLine($"El producto {nombreProducto} fue encontrado:");
+                Console.WriteLine(productoDocument);
+            }
+            else
+            {
+                Console.WriteLine($"No se encontró ningún producto con el nombre {nombreProducto}");
+            }
+        }
+
+        static void BuscarMarcaPorNombre()
+        {
+            Console.WriteLine("Ingrese el nombre de la marca:");
+            string nombreMarca = Console.ReadLine();
+
+            var filter = Builders<BsonDocument>.Filter.Eq("Nombre", nombreMarca);
+            var marcaDocument = marcasCollection.Find(filter).FirstOrDefault();
+
+            if (marcaDocument != null)
+            {
+                Console.WriteLine($"La marca {nombreMarca} fue encontrada:");
+                Console.WriteLine(marcaDocument);
+            }
+            else
+            {
+                Console.WriteLine($"No se encontró ninguna marca con el nombre {nombreMarca}");
+            }
+        }
+
         static void EliminarProducto()
         {
             Console.WriteLine("Ingrese el nombre del producto que desea eliminar:");
@@ -183,54 +229,52 @@ namespace MongoDBExample
         }
 
         static void ModificarProducto()
-{
-    Console.WriteLine("Ingrese el nombre del producto que desea modificar:");
-    string producto = Console.ReadLine();
-
-    var filter = Builders<BsonDocument>.Filter.Eq("Nombre", producto);
-    var productoDocument = productosCollection.Find(filter).FirstOrDefault();
-
-    if (productoDocument != null)
-    {
-        Console.WriteLine("Ingrese el nuevo nombre del producto:");
-        string nuevoNombre = Console.ReadLine();
-
-        Console.WriteLine("Ingrese el nuevo stock:");
-        int nuevoStock = int.Parse(Console.ReadLine());
-
-        var idMarca = productoDocument.GetValue("IdMarca").AsString;
-        var marcaFilter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(idMarca));
-        var marcaDocument = marcasCollection.Find(marcaFilter).FirstOrDefault();
-
-        if (marcaDocument != null)
         {
-            var productosArray = marcaDocument.GetValue("Productos").AsBsonArray;
-            for (int i = 0; i < productosArray.Count; i++)
+            Console.WriteLine("Ingrese el nombre del producto que desea modificar:");
+            string producto = Console.ReadLine();
+
+            var filter = Builders<BsonDocument>.Filter.Eq("Nombre", producto);
+            var productoDocument = productosCollection.Find(filter).FirstOrDefault();
+
+            if (productoDocument != null)
             {
-                if (productosArray[i].AsString == producto)
+                Console.WriteLine("Ingrese el nuevo nombre del producto:");
+                string nuevoNombre = Console.ReadLine();
+
+                Console.WriteLine("Ingrese el nuevo stock:");
+                int nuevoStock = int.Parse(Console.ReadLine());
+
+                var idMarca = productoDocument.GetValue("IdMarca").AsString;
+                var marcaFilter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(idMarca));
+                var marcaDocument = marcasCollection.Find(marcaFilter).FirstOrDefault();
+
+                if (marcaDocument != null)
                 {
-                    productosArray[i] = nuevoNombre;
-                    break;
+                    var productosArray = marcaDocument.GetValue("Productos").AsBsonArray;
+                    for (int i = 0; i < productosArray.Count; i++)
+                    {
+                        if (productosArray[i].AsString == producto)
+                        {
+                            productosArray[i] = nuevoNombre;
+                            break;
+                        }
+                    }
+
+                    var updateMarca = Builders<BsonDocument>.Update.Set("Productos", productosArray);
+                    marcasCollection.UpdateOne(marcaFilter, updateMarca);
                 }
+
+                var updateProducto = Builders<BsonDocument>.Update
+                    .Set("Nombre", nuevoNombre)
+                    .Set("Stock", nuevoStock);
+
+                productosCollection.UpdateOne(filter, updateProducto);
+                Console.WriteLine($"Producto {producto} modificado correctamente.");
             }
-
-            var updateMarca = Builders<BsonDocument>.Update.Set("Productos", productosArray);
-            marcasCollection.UpdateOne(marcaFilter, updateMarca);
+            else
+            {
+                Console.WriteLine($"No se encontró el producto {producto}");
+            }
         }
-
-        var updateProducto = Builders<BsonDocument>.Update
-            .Set("Nombre", nuevoNombre)
-            .Set("Stock", nuevoStock);
-
-        productosCollection.UpdateOne(filter, updateProducto);
-        Console.WriteLine($"Producto {producto} modificado correctamente.");
-    }
-    else
-    {
-        Console.WriteLine($"No se encontró el producto {producto}");
-    }
-}
-
-
     }
 }
